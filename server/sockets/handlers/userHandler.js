b34f242af10060ccc22c73133f72b38b46ee3dd8
Leonid Lichkun <lionproklon@gmail.com>
2022-11-22 16:33:18 +0300

    Realized handlers for sockets for event with user...

--- server/sockets/handlers/userHandler.js
+++ server/sockets/handlers/userHandler.js
@@ -1,3 +1,38 @@
-module.exports = () => {
-    console.log("userHandler");
+const { logMagenta } = require("../../config/utils/styledLogs");
+
+const users = {};
+
+module.exports = (io, socket) => {
+    logMagenta("some actions with users");
+    const { roomId, userName } = socket;
+
+    if (!users[roomId]) {
+        users[roomId] = [];
+    }
+
+    const updateUserList = () => {
+        io.to(roomId).emit("users_list:update", users[roomId]);
+    };
+
+    socket.on("user:add", (user) => {
+        socket.to(roomId).emit("log", `User ${userName} connected`);
+
+        user.socketId = socket.id;
+
+        users[roomId].push(user);
+
+        updateUserList();
+    });
+
+    socket.on("disconnect", () => {
+        if (!users[roomId]) return;
+
+        socket.to(roomId).emit("log", `User ${userName} disconnected`);
+
+        users[roomId] = users[roomId].filter(
+            (user) => user.socketId !== socket.id
+        );
+
+        updateUserList();
+    });
 };
