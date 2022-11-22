c08edf5fa35be4491575c011aa2b9a9bc1e5f853
Leonid Lichkun <lionproklon@gmail.com>
2022-11-22 16:38:40 +0300

    Created structure for work with messages and DB

--- server/sockets/handlers/messageHandler.js
+++ server/sockets/handlers/messageHandler.js
@@ -1,3 +1,30 @@
+const { logMagenta, logBgWhite } = require("../../config/utils/styledLogs");
+
+const messages = {};
+
 module.exports = () => {
-    console.log("messageHandler");
+    logMagenta("working with messageS");
+    const { roomId } = socket;
+
+    const updateMessageList = () => {
+        io.to(roomId).emit("message_list:update", messages[roomId]);
+    };
+
+    socket.on("message:get", () => {
+        logBgWhite("Get messages from DB");
+    });
+
+    socket.on("message:add", (message) => {
+        logBgWhite("Create message at DB");
+    });
+
+    socket.on("message:remove", (message) => {
+        const { messageId, messageType, textOrPathToFile } = message;
+        logBgWhite("Delete message from DB");
+        messages[roomId] = messages[roomId].filter(
+            (message) => message.messageId !== messageId
+        );
+
+        updateMessageList();
+    });
 };
